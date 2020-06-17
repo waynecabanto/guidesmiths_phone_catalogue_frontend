5fd08d5290defb86e369737f20c4551e58015e9f
waynecabanto <cabantowayne@gmail.com>
2020-06-17 03:15:06 +0200

    second commit with build

---
+++ build/precache-manifest.de50f5957bd50385975ed9372fed985e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb17c06a31dd561d353a272148b0bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa4bf354defd35d4e7e",
+    "url": "/static/css/main.112ffd6f.chunk.css"
+  },
+  {
+    "revision": "53d04226d7c0379071f1",
+    "url": "/static/js/2.5d526cde.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5d526cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa4bf354defd35d4e7e",
+    "url": "/static/js/main.2791ee98.chunk.js"
+  },
+  {
+    "revision": "bf6468208d4d57ef946d",
+    "url": "/static/js/runtime-main.f121cd7f.js"
+  }
+]);
